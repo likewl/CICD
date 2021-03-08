f433f486a3a17cd676ecb90c0f32b8ab5147866b
like <535889315@qq.com>
2021-03-08 18:33:50 +0800

    213123

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 
 func main() {
 	http.HandleFunc("/a", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("时间1"))
+		w.Write([]byte("时间12"))
 	})
 	err := http.ListenAndServe(":9090", nil)
 	if err != nil {
