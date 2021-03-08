c568104969f06a609dd91a5c132ae5d965e4e279
like <535889315@qq.com>
2021-03-08 14:47:45 +0800

    基本代码完成

---
+++ main.go
@@ -0,0 +1,22 @@
+/**
+ * @author  like
+ * @date  2021/3/8 12:52
+ * @description CI/CD测试程序
+ */
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/a", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hhhhh"))
+	})
+	err := http.ListenAndServe(":9090", nil)
+	if err != nil {
+		fmt.Println("HTTP server failed,err:", err)
+		return
+	}
+}
