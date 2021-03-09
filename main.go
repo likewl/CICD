a626b231ac61171cd2a96ce1f30aa1b90b06e3c4
like <535889315@qq.com>
2021-03-09 12:14:18 +0800

    测试

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 func main() {
 	http.HandleFunc("/a", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(time.Now().Format("2006-01-02 15:04:05"))
-		w.Write([]byte("时间12344"))
+		w.Write([]byte(time.Now().Format("2006-01-02 15:04:05")))
 	})
 	err := http.ListenAndServe(":9090", nil)
 	if err != nil {
