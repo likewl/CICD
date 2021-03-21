b419238ffff1fb9153988cf1269cc4db607ef8a3
like <535889315@qq.com>
2021-03-21 19:33:15 +0800

    优化cicd代码

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,9 @@
-FROM golang:alpine
+FROM scratch
 
-# 为我们的镜像设置必要的环境变量
-ENV GO111MODULE=on \
-    CGO_ENABLED=0 \
-    GOOS=linux \
-    GOARCH=amd64
-
-# 移动到工作目录：/build
-WORKDIR /build
-
-# 将代码复制到容器中
-
-COPY . .
-# 将我们的代码编译成二进制可执行文件app
-RUN go build -o app .
-
+# 将运行程序和日志复制到容器中
+COPY test .
 # 声明服务端口
 EXPOSE 9090
 
 # 启动容器时运行的命令
-CMD ["/build/app"]
+CMD ["/test"]
--- main.go
+++ main.go
@@ -7,11 +7,20 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
 func main() {
+	open, err2 := os.OpenFile("log/test.log",os.O_RDWR,0666)
+	if err2 != nil {
+		fmt.Println(err2)
+	}
+	log.Default()
+	logger := log.New(open,"",log.LstdFlags)
+	logger.Println("log")
 	http.HandleFunc("/a", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(time.Now().Format("2006-01-02 15:04:05"))
 		w.Write([]byte(time.Now().Format("2006-01-02 15:04:05")))
